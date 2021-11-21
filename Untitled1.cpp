bfe0d53b382c7bd90369e3616d9a8a50e9897725
jacksonchen <56949000+jacksonchen1998@users.noreply.github.com>
2021-11-21 14:30:40 +0800

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,29 @@
+# include <iostream>
+
+using namespace std;
+
+void BinarySearch(int* storage, int x, int num, int low, int high){
+	int middle = (low + high) / 2;
+	if(storage[middle] == x){
+		cout << "Found " << x <<" in the index " << middle <<"\n";
+	}else if(storage[middle] > x){
+		BinarySearch(storage, x, num, low, middle - 1);
+	}else if(storage[middle] < x){
+		BinarySearch(storage, x, num, middle + 1, high);
+	}else{
+		cout << "Couldn't find " << x <<"\n";
+	}
+} 
+
+int main(){
+	int number = 10, x;
+	int storage[number] = {-15, -5, 0, 1, 3, 5, 7, 9, 13, 14};
+	for(int i = 0; i < number; i++)
+		cout << storage[i] <<" ";
+	cout << "\n";
+	cout << "Find : ";
+	cin >> x;
+	BinarySearch(storage, x, number, 0, number - 1);
+	system("PAUSE");
+	return 0;
+}
